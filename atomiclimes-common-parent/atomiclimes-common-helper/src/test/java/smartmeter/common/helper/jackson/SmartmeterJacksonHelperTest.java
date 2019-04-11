19a83cced21767b42867e942675d67694163bddb
Mirko Pohland <post@mpohland.de>
2019-04-11 19:11:22 +0200

    added frontend javascript files

---
+++ atomiclimes-common-parent/atomiclimes-common-helper/src/test/java/smartmeter/common/helper/jackson/SmartmeterJacksonHelperTest.java
@@ -0,0 +1,53 @@
+package smartmeter.common.helper.jackson;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SmartmeterJacksonHelperTest {
+
+	private static final String JSON_STRING = "{\"firstString\":\"firstString\",\"secondString\":\"secondString\"}";
+	private SmartmeterJacksonHelper smartmeterJacksonHelper;
+
+	@Before
+	public void setup() {
+		this.smartmeterJacksonHelper = new SmartmeterJacksonHelper(TestClass.class);
+
+	}
+
+	@Test
+	public void serializeTest() {
+		String jsonString = smartmeterJacksonHelper.serialize(new TestClass());
+		assertEquals(JSON_STRING, jsonString);
+	}
+
+	@Test
+	public void deserializeTest() {
+		TestClass testClass = (TestClass) smartmeterJacksonHelper.deserialize(JSON_STRING);
+		assertEquals("firstString", testClass.getFirstString());
+	}
+
+	public static class TestClass {
+		private String firstString = "firstString";
+		private String secondString = "secondString";
+
+		public String getFirstString() {
+			return firstString;
+		}
+
+		public void setFirstString(String firstString) {
+			this.firstString = firstString;
+		}
+
+		public String getSecondString() {
+			return secondString;
+		}
+
+		public void setSecondString(String secondString) {
+			this.secondString = secondString;
+		}
+
+	}
+
+}
---
+++ atomiclimes-web-parent/atomiclimes-web-wicket/src/main/java/smartmeter/web/gui/ProductionItemListView.java
@@ -0,0 +1,51 @@
+package smartmeter.web.gui;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+
+import smartmeter.common.dao.entities.Packaging;
+import smartmeter.common.dao.entities.ProductionItem;
+import smartmeter.common.helper.jackson.SmartmeterJacksonHelper;
+
+public class ProductionItemListView extends ListView<ProductionItem> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private SmartmeterJacksonHelper smartmeterJacksonHelper;
+
+	public ProductionItemListView(String id, List<ProductionItem> list) {
+		super(id, list);
+		this.smartmeterJacksonHelper = new SmartmeterJacksonHelper(ProductionItem.class);
+	}
+
+	@Override
+	protected void populateItem(ListItem<ProductionItem> item) {
+		final ProductionItem productionItem = item.getModelObject();
+		productionItem.getProduct();
+		List<Packaging> packagingList = productionItem.getPackaging();
+		Iterator<Packaging> packagingIterator = packagingList.iterator();
+		while (packagingIterator.hasNext()) {
+			packagingIterator.next();
+		}
+		
+		String productionItemJSON = this.smartmeterJacksonHelper.serialize(productionItem);
+		System.out.println(productionItemJSON);
+
+		item.add(new Label("product", productionItemJSON));
+
+//		String[] packagingOrderTypes = { "1", "2", "3" };
+
+//		for (String packagingOrder : packagingOrderTypes) {
+//			String packagingName = "";
+
+//			item.add(new Label("packagingOfOrder" + packagingOrder, packagingName));
+//		}
+	}
+
+}
