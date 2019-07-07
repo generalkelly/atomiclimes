5e629de1b306f0a376cf23ef5968d26615daf801
Mirko Pohland <post@mpohland.de>
2019-07-07 09:13:15 +0200

    added missing folder structure to atomiclimes-agent

---
+++ atomiclimes-agent/src/main/java/io/atomiclimes/agent/AtomicLimesAgent.java
@@ -0,0 +1,13 @@
+package io.atomiclimes.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomicLimesAgent {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtomicLimesAgent.class, args);
+	}
+
+}
